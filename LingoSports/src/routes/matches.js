83d67e992c0c02d76d08e9baac865d1c39a9e4ef
Maulik Ranadive <maulikranadive355@gmail.com>
2026-02-22 15:25:51 +0530

    implemented the websocketserver

--- LingoSports/src/routes/matches.js
+++ LingoSports/src/routes/matches.js
@@ -5,6 +5,7 @@ import { createMatchSchema, listMatchesQuerySchema } from '../validation/matches
 import { getMatchStatus } from '../utils/match-status.js';
 import { desc } from 'drizzle-orm';
 
+
 export const matchRouter = Router();
 const MAX_LIMIT = 100;
 
@@ -59,6 +60,10 @@ matchRouter.post('/', async (req, res) => {
         status: getMatchStatus(startTime, endTime),
       })
       .returning();
+
+      if(res.app.locals.broadCastMatchCreated){
+        res.app.locals.broadCastMatchCreated(event);
+      }
 
     return res.status(201).json({ data: event });
   } catch (error) {
